6d81879554b8c5acc831fb107edc32aedaeb231f
jsprofessional <jshuman@protonmail.com>
2022-04-24 13:31:43 -0500

    Added bootstrap. Removed stock css

--- app/src/components/Navbar.vue
+++ app/src/components/Navbar.vue
@@ -1,3 +1,11 @@
 <template>
-	<h1>BANGKOK INN</h1>
+	<nav class="navbar sticky-top navbar-light bg-warning">
+		<div class="container-fluid">
+			<a class="navbar-brand" href="#">BANGKOK INN</a>
+		</div>
+	</nav>
+			<h1>BANGKOK INN</h1>
+			<h2>MENU</h2>
+			<h2>ABOUT</h2>
+			<h2>ORDER</h2>
 </template>
--- vue-bangkok/src/components/Navbar.vue
+++
@@ -1,3 +0,0 @@
-<template>
-	<h1>BANGKOK INN</h1>
-</template>
