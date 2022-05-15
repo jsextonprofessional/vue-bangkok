d205e72453d33d306993eddb04db3bd8ec27cc94
jsprofessional <jshuman@protonmail.com>
2022-05-15 12:25:39 -0500

    Added Tailwind to app

--- app/src/App.vue
+++ app/src/App.vue
@@ -28,4 +28,10 @@ import Navbar from "@/components/Navbar.vue";
 
   <RouterView /> -->
   <h1>YEET</h1>
+  <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
 </template>
+
+<style src="@/assets/css/styles.css">
+</style>
