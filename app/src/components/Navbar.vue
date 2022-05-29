589217c0cb2d62651efa3c04b064a063c807af89
jsprofessional <jshuman@protonmail.com>
2022-05-29 13:44:14 -0500

    Refactored navbar component to use button instead of divs.

--- app/src/components/Navbar.vue
+++ app/src/components/Navbar.vue
@@ -5,40 +5,40 @@ import { RouterLink } from "vue-router";
 <template>
 	<div class="bg-white border-8 border-black flex justify-between px-20 py-8">
 		<div>
-			<div class="border-8 border-transparent hover:border-black px-6 py-2">
 				<RouterLink
 					to="/"
 					class="font-medium hover:text-cyan-300 text-4xl text-rose-400"
 				>
-					BANGKOK INN
+					<button class="border-8 border-transparent hover:border-black px-6 py-2">
+						BANGKOK INN
+					</button>
 				</RouterLink>
-			</div>
 		</div>
 		<div class="flex gap-10">
-			<div class="border-8 border-transparent hover:border-black px-3 py-2">
 				<RouterLink
 					to="/menu"
 					class="hover:text-cyan-300 text-4xl text-rose-300"
 				>
-					MENU
+					<button class="border-8 border-transparent hover:border-black px-3 py-2">
+						MENU
+					</button>
 				</RouterLink>
-			</div>
-			<div class="border-8 border-transparent hover:border-black px-3 py-2">
 				<RouterLink
 					to="/about"
 					class="hover:text-cyan-300 text-4xl text-rose-300"
 				>
-					ABOUT
+					<button class="border-8 border-transparent hover:border-black px-3 py-2">
+						ABOUT
+					</button>
 				</RouterLink>
-			</div>
-			<div class="border-8 border-transparent hover:border-black px-3 py-2">
 				<RouterLink
 					to="/order"
 					class="hover:text-cyan-300 text-4xl text-rose-300"
 				>
-					ORDER
+					<button class="border-8 border-transparent hover:border-black px-3 py-2">
+						ORDER
+					</button>
 				</RouterLink>
-			</div>
 		</div>
 	</div>
 </template>
