98409f4c9a372948a72505e734388b92832d9538
jsprofessional <jshuman@protonmail.com>
2022-06-12 12:48:10 -0500

    Styled appetizers.

--- app/src/components/menu/Appetizers.vue
+++ app/src/components/menu/Appetizers.vue
@@ -1,22 +1,59 @@
 <template>
-	<div>
-		<h1 class="text-center">Appetizers</h1>
-		<div class="border-4 border-rose-400 mx-12 rounded-full">
-			<div class="flex justify-around">
-				<div class="">
-					<h4>Corn Patties (4) $7.50</h4>
-					<h4>Fried Thai Egg Roll (3) $5.50</h4>
-					<h4>Fried Vegetable Egg Rolls (3) $5.50</h4>
-					<h4>Spring Roll (3) Vegetable $5.50</h4>
-					<h4>Spring Roll (3) Shrimp $6.50</h4>
-					<h4>Thai Dumplings (5) $6.50</h4>
+	<div class="my-8">
+		<div class="align-middle bg-black grid place-content-center h-20 mx-auto my-8 px-16 w-60">
+			<h1 class="font-semibold text-center text-4xl text-white">Appetizers</h1>
+		</div>
+		<div class="border-8 border-black hover:border-cyan-400 mx-12">
+			<div class="divide-x-8 divide-black divide-dashed hover:divide-rose-400 grid grid-cols-2 p-8">
+				<div class="space-y-4 mx-8">
+					<!-- LEFT COLUMN -->
+					<div class="flex justify-between">
+						<h4 class="font-medium text-lg">Corn Patties (4)</h4>
+						<h4 class="font-medium text-lg">$7.50</h4>
+					</div>
+					<div class="flex justify-between">
+						<h4 class="font-medium text-lg">Fried Thai Egg Roll (3)</h4>
+						<h4 class="font-medium text-lg">$5.50</h4>
+					</div>
+					<div class="flex justify-between">
+						<h4 class="font-medium text-lg">Fried Vegetable Egg Rolls (3)</h4>
+						<h4 class="font-medium text-lg">$5.50</h4>
+					</div>
+					<div class="flex justify-between">
+						<h4 class="font-medium text-lg">Spring Roll (3) Vegetable</h4>
+						<h4 class="font-medium text-lg">$5.50</h4>
+					</div>
+					<div class="flex justify-between">
+						<h4 class="font-medium text-lg">Spring Roll (3) Shrimp</h4>
+						<h4 class="font-medium text-lg">$6.50</h4>
+					</div>
+					<div class="flex justify-between">
+						<h4 class="font-medium text-lg">Thai Dumplings (5)</h4>
+						<h4 class="font-medium text-lg">$6.50</h4>
+					</div>
 				</div>
-				<div>
-					<h4>Spicy Fried Shrimp (7) $8.50</h4>
-					<h4>Satay Chicken or Pork (5) $7.50</h4>
-					<h4>Satay Shrimp (5) $8.50</h4>
-					<h4>Tofu Satay (5) $7.50</h4>
-					<h4>Fried Tofu $5.50</h4>
+				<!-- RIGHT COLUMN -->
+				<div class="px-8 space-y-4">
+					<div class="flex justify-between">
+						<h4 class="font-medium text-lg">Spicy Fried Shrimp (7)</h4>
+						<h4 class="font-medium text-lg">$8.50</h4>
+					</div>
+					<div class="flex justify-between">
+						<h4 class="font-medium text-lg">Satay Chicken or Pork (5)</h4>
+						<h4 class="font-medium text-lg">$7.50</h4>
+					</div>
+					<div class="flex justify-between">
+						<h4 class="font-medium text-lg">Satay Shrimp (5)</h4>
+						<h4 class="font-medium text-lg">$8.50</h4>
+					</div>
+					<div class="flex justify-between">
+						<h4 class="font-medium text-lg">Tofu Satay (5)</h4>
+						<h4 class="font-medium text-lg">$7.50</h4>
+					</div>
+					<div class="flex justify-between">
+						<h4 class="font-medium text-lg">Fried Tofu</h4>
+						<h4 class="font-medium text-lg">$5.50</h4>
+					</div>
 				</div>
 			</div>
 		</div>
